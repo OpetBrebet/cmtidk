b99f5f2838954db575610c485a72f31347d60b9d
OpetBrebet <rainerkenan@gmail.com>
2026-04-29 15:56:56 +0700

    feat: add ui on home page

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react"
-
 import { useAuth } from "../hooks/useAuth"
 import { useApi } from "../lib/api"
+
+import "./Home.css"
+import { Add, Assignment, DeleteForever } from "@mui/icons-material"
+import { useNavigate } from "react-router-dom"
 
 type DocumentSummary = {
     id: string
@@ -11,6 +14,7 @@ type DocumentSummary = {
 
 export default function Home() {
     const [documents, setDocuments] = useState<DocumentSummary[]>([])
+    const navigate = useNavigate()
     const { isSignedIn } = useAuth()
     const { getDocuments, deleteDocument } = useApi()
 
@@ -31,22 +35,37 @@ export default function Home() {
     }
 
     return (
-        <>
-            <div className="document-list-bar">
-                <a href={`/editor/create`}>New Document</a>
+        <div className="document-list-body">
+            <div className="list-top-bar">
+                <button
+                    className="document-create"
+                    onClick={() => navigate('/editor/create')}
+                >
+                    <Add />
+                    New Document
+                </button>
             </div>
-            <div className="documentList">
-                <ul>
-                    {documents.map(doc => (
-                        <li key={doc.id} className="documentItem">
-                            <a href={`/editor/${doc.id}`}>{doc.title} - {doc.artist}</a>
-                            <button onClick={() => onDeleteRequest(doc.id)}>
-                                Delete Document
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </>
+            <ul className="document-list">
+                <li className="document-list-title">
+                    <span className="document-list-title-name">Name</span>
+                </li>
+                {documents.map(doc => (
+                    <li
+                        key={doc.id}
+                        className="document-item"
+                        onClick={() => navigate(`/editor/${doc.id}`)}
+                    >
+                        <Assignment />
+                        <div className="document-item-name">
+                            <span className="document-item-title">{doc.title}</span>
+                            <span className="document-item-artist">{doc.artist}</span>
+                        </div>
+                        <button className="document-delete-button" onClick={() => onDeleteRequest(doc.id)}>
+                            <DeleteForever />
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
